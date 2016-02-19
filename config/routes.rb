01dc1ca16efe5f2ffd1de96463b823409345d0aa
dma315 <dma315@gmail.com>
2016-02-18 19:37:22 -0600

    Add experience controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root to: 'application#angular'
 
+  resources :users do
+    resources :experiences do
+      resources :assets
+    end
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
