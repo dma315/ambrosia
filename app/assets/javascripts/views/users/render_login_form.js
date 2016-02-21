11cb4c1d2239d8de251012712d445c07443c28e0
Calvin Lang <email": null>
2016-02-20 20:16:42 -0600

    Registration done

--- app/assets/javascripts/views/users/render_login_form.js
+++ app/assets/javascripts/views/users/render_login_form.js
@@ -1,5 +1,12 @@
 function renderLoginForm() {
   if ($(document).find('#user-login-box').length === 0 && $(document).find('#user-registration-box').length === 0) {
+    // $('body > *').not("#user-login-box").on('click', function(){
+    //   $('#user-login-box').fadeOut()
+    //   function removeLoginBox() {
+    //     $('#user-login-box').remove()
+    //   }
+    //   setTimeout(removeLoginBox, 1000)
+    // })
     $.ajax({
       method: "GET",
       url: "/sessions/new"
--- app/assets/javascripts/views/users/user_registration_form.js
+++ app/assets/javascripts/views/users/user_registration_form.js
@@ -1,12 +1,12 @@
 var userRegister = function(){
   if ($(document).find('#user-login-box').length === 0 && $(document).find('#user-registration-box').length === 0) {
-    $('body > *').not("#user-registration-box").on('click', function(){
-      $('#user-registration-box').fadeOut()
-      function removeRegistrationBox() {
-        $('#user-registration-box').remove()
-      }
-      setTimeout(removeRegistrationBox, 1000)
-    })
+    // $('body > *').not("#user-registration-box").on('click', function(){
+    //   $('#user-registration-box').fadeOut()
+    //   function removeRegistrationBox() {
+    //     $('#user-registration-box').remove()
+    //   }
+    //   setTimeout(removeRegistrationBox, 1000)
+    // })
     $.ajax({
       method: "GET",
       url: "/users/new"
