c68966f98681e205de98a12a9c4bf875cb7f73bc
David Ma <email": null>
2016-02-21 17:20:40 -0600

    Refactor create form submission

--- app/assets/javascripts/views/experiences/send_create_experience.js
+++ app/assets/javascripts/views/experiences/send_create_experience.js
@@ -6,16 +6,7 @@ function createExperience(form){
     data: formData
   })
   request.done(function(response){
-    console.log("Response")
-    console.log(response)
-    $('#experience-box').fadeOut()
-    function removeRegistrationBox() {
-      $('#experience-box').remove()
-    }
-    setTimeout(removeRegistrationBox, 1000)
-    $('#main-menu').find('#register').remove()
-    $('#main-menu').find('#login').remove()
-    $('#main-menu ul li:last').append("<a id='logout' href='#/logout'>Logout</a>");
+    clearMainFrame();
   })
   request.fail(function(response) {
     console.log('nope')
