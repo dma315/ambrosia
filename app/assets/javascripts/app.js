eb1a19f531197d00fc1bb70ad67b7e5cab3f396e
David Ma <email": null>
2016-02-21 13:23:27 -0600

    Add ability to switch between login and regsitration

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
   });
 
   // Login stuff
-  $('#main-menu').on('click', '#login', function() {
+  $(document).on('click', '#login', function() {
     renderLoginForm()
   });
 
