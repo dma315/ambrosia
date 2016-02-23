01c37a9f31c200019f6110fa14640ba23c2a955b
dma315 <dma315@gmail.com>
2016-02-22 22:31:44 -0600

    Fix bugs in migrations; try to fix overflow bubble issue

--- db/migrate/20160222193745_remove_columns_from_assets.rb
+++ db/migrate/20160222193745_remove_columns_from_assets.rb
@@ -2,9 +2,9 @@ class RemoveColumnsFromAssets < ActiveRecord::Migration
   def change
     remove_column :assets, :link
     remove_column :assets, :processed
-    remove_column :assets, :upload_file_name
-    remove_column :assets, :upload_content_type
-    remove_column :assets, :upload_file_size
-    remove_column :assets, :upload_updated_at
+    # remove_column :assets, :upload_file_name
+    # remove_column :assets, :upload_content_type
+    # remove_column :assets, :upload_file_size
+    # remove_column :assets, :upload_updated_at
   end
 end
