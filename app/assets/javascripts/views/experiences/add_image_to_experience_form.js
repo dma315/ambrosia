769520bf1ac90987d942cec4e08bd78b34480c5c
David Ma <email": null>
2016-02-22 19:10:16 -0600

    Add submit button to upload images dialog

--- app/assets/javascripts/views/experiences/add_image_to_experience_form.js
+++ app/assets/javascripts/views/experiences/add_image_to_experience_form.js
@@ -15,6 +15,7 @@ function addImageToExperienceForm(experienceID) {
     })
     $dropzone = $(response)
     $dropzone.find('#multi-upload').append($experienceID)
+    $submit = $("<input type='submit'>").addClass("form-submit").appendTo($dropzone)
     appendToMainFrame($dropzone)
     var dropzone = new Dropzone("#multi-upload", { url: postURL });
     // s3Run()
