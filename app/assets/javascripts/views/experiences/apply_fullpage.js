74e728960f708c6fd08f1ccea4a4da0fad72929f
Calvin Lang <langcalvin@gmail.com>
2016-02-23 15:43:46 -0600

    Can now change experiences and refresh panel navigation

--- app/assets/javascripts/views/experiences/apply_fullpage.js
+++ app/assets/javascripts/views/experiences/apply_fullpage.js
@@ -1,14 +1,21 @@
 var applyFullpage = function() {
+	removePanelNavigation()
 	var fakeNavs = new Array
-	for (var i = 0; i < 150; i++) { fakeNavs.push("panel") }
+	for (var i = 1; i < 150; i++) { fakeNavs.push("Panel " + i) }
   $('html').removeClass('fp-enabled').promise().done(function(){
     $('#fullpage').fullpage({
     	scrollOverflow: true,
     	loopBottom: true,
     	anchors: fakeNavs,
 		navigation: true,
-		// navigationPosition: 'right',
+		navigationPosition: 'right',
 		navigationTooltips: fakeNavs,
     });
   })
+};
+
+var removePanelNavigation = function(){
+	if ($("#fp-nav")) {
+		$("#fp-nav").remove()
+	};
 };
