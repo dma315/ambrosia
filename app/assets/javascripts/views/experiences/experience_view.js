2437bd0b48c571543fb093a828e9fb69ba0fd958
Steven Jones <steven.g.jones88@gmail.com>
2016-02-24 10:40:14 -0600

    Clean up cruft in experience_view.js and masonry.css

--- app/assets/javascripts/views/experiences/experience_view.js
+++ app/assets/javascripts/views/experiences/experience_view.js
@@ -3,13 +3,6 @@ function ExperienceView(id) {
   this.$element = $("<div>").addClass('module grid');
   this.id = id;
   this.masonify();
-  // Some sort of object that holds "sections"
-  // Some algorithm that determines which pictures go into which "section"
-}
-
-ExperienceView.prototype.viewSelector = function() {
-  var $selector = $('<div>').attr('id', 'selector')
-
 }
 
 ExperienceView.prototype.render = function() {
@@ -20,17 +13,13 @@ ExperienceView.prototype.render = function() {
   var $gridSizer = $('<div>').addClass('grid-sizer');
   this.$element.append($gridSizer);
   var sectionifier = new Sectionify(this.id);
-  // sectionifier.thingsToRender.push($bigPicSection.wrap("<div class='grid-item'></div>"));
-
   sectionifier.buildMasonryPage(sectionifier.assets);
   sectionifier.thingsToRender.forEach(function(renderable){
     that.$element.append(renderable);
-    // appendToFullPage($bigPicSection);
   });
 
   clearFullpage().done(function(){
     appendToFullPage(that.$element.addClass("section")).done(that.remasonify.bind(that));
-    // appendToFullPage($bigPicSection);
   });
   this.$element.kinetic();
 }
@@ -39,19 +28,14 @@ ExperienceView.prototype.masonify = function() {
   var that = this;
   this.$element.imagesLoaded(function() {
     that.responsify();
-//     that.$element.masonry({
-//       itemSelector: '.grid-item',
-//       columnWidth: '.grid-sizer',
-//       gutter: 10,
-//       percentPosition: true
-    });
-//   });
+  });
 }
 
 ExperienceView.prototype.remasonify = function() {
-  // this.$element.masonry("layout");
-  this.responsify();
-  // applyFullpage();
+  var that = this;
+  this.$element.imagesLoaded(function(){
+    that.responsify();
+  })
 };
 
 ExperienceView.prototype.responsify = function() {
