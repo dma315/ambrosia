a4cc1bc4faeae5ca3ade93f4455ca6adc946d24f
David Ma <email": null>
2016-02-25 11:32:36 -0600

    Commit feature complete

--- app/assets/javascripts/views/experiences/experience_view.js
+++ app/assets/javascripts/views/experiences/experience_view.js
@@ -14,22 +14,24 @@ ExperienceView.prototype.loadAssets = function(_arrayOfLayouts) {
   var assetCount = this.assetCount
   var assetsPaneled = this.assetsPaneled
   var layoutLookup = this.layoutLookup
-  var assets = this.assets
   var panels = this.panels
 
+  // Custom order assets to what user specifies
+  this.assets = this.reorderAssets()
+  var reorderedAssets = this.assets
 
   if (arrayOfLayouts) {
     // Take first layout
     arrayOfLayouts.forEach(function(layoutOption) {
       if (layoutOption.constructor == String) {
         var numAssets = layoutLookup[layoutOption][1]
-        var assetSlice = assets.slice(assetsPaneled, assetsPaneled + numAssets)
+        var assetSlice = reorderedAssets.slice(assetsPaneled, assetsPaneled + numAssets)
         var panel = new PanelView(assetSlice)
         var loadedPanel = eval("panel." + layoutOption + "()")
       } else if (layoutOption.constructor == Array) {
         var numAssets = layoutOption[1]
         var method = layoutOption[0]
-        var assetSlice = assets.slice(assetsPaneled, assetsPaneled + numAssets)
+        var assetSlice = reorderedAssets.slice(assetsPaneled, assetsPaneled + numAssets)
         var panel = new PanelView(assetSlice)
         var loadedPanel = eval("panel." + method + "(" + numAssets + ")")
       }
@@ -60,8 +62,10 @@ ExperienceView.prototype.loadRemainingAssets = function(startingIndex) {
 
   var assetsIndex = startingIndex
   while (assetsIndex < assetCount) {
-    var panel = new PanelView([assets[assetsIndex]])
-    panels.push(panel.loadSingleImage())
+    if (assets[assetsIndex]) {
+      var panel = new PanelView([assets[assetsIndex]])
+      panels.push(panel.loadSingleImage())
+    }
     assetsIndex += 1
   }
 }
@@ -70,16 +74,20 @@ ExperienceView.prototype.loadRemainingAssets = function(startingIndex) {
 ExperienceView.prototype.render = function() {
   removePanelNavigation()
   // For testing a predefined set of routes
-  this.loadAssets(["loadSingleImage", "loadTwoImages"])
-  this.loadAssets([
-    ["titleCaptionWithOverflow", 4], //Includes overflow and "loadSingleImage" should be next
-    "loadSingleImage",
-    ["imagesWithCaptions", 8],
-    "titleCaption",
-    ["masonify", 8],
-    ["masonifyWithCaptions", 10],
-    "loadTwoImages"])
-  // this.loadAssets(this.panelInstructions)
+  // this.loadAssets(["loadSingleImage", "loadTwoImages"])
+  // this.loadAssets([
+  //   ["titleCaptionWithOverflow", 4], //Includes overflow and "loadSingleImage" should be next
+  //   "loadSingleImage",
+  //   ["imagesWithCaptions", 8],
+  //   "titleCaption",
+  //   ["masonify", 8],
+  //   ["masonifyWithCaptions", 10],
+  //   "loadTwoImages"])
+  var panelRotation = []
+  this.panelInstructions.forEach(function(element) {
+    panelRotation.push(element[0])
+  })
+  this.loadAssets(panelRotation)
 
   this.panels.forEach(function(panel) {
     $('#fullpage').append(panel)
@@ -96,8 +104,47 @@ ExperienceView.prototype.getPanels = function() {
     url: "/experiences/" + experience.id + "/panels.json"
   })
   return request.then(function(response) {
+    console.log(response)
     response.forEach(function(panelInstruction) {
       thisView.panelInstructions.push(panelInstruction)
     })
   })
 }
+
+ExperienceView.prototype.reorderAssets = function() {
+  var assets = this.assets
+  var reorderedAssets = []
+  var panelInstructions = this.panelInstructions
+
+  function findAssetByID(id) {
+    return assets.find(function(element) {
+      return element.id === id
+    })
+  }
+
+  panelInstructions.forEach(function(arrayOfAssets) {
+    // Array[0] is the instruction
+    // Array[1] is the set of asset IDs
+    arrayOfAssets[1].forEach(function(assetID) {
+      asset = findAssetByID(assetID)
+      var index = assets.indexOf(asset)
+      reorderedAssets.push(asset)
+      assets.splice(index, 1)
+    })
+  })
+
+  assets = reorderedAssets.concat(assets)
+  console.log(assets)
+  return assets
+
+}
+
+// The ordering is a bit messed up here
+
+// The sequence is --
+// When a bubble is clicked
+// The bubble does an AJAX request to get the panel ordering from the database
+// When that is DONE (on promise)
+// Render is called
+//    Which loads the assets via loadAssets (accepting the ordered panel array)
+//    This is weird because embedded inside loadAssets is a reorder assets function that orders the assets correctly (critical)
--- app/helpers/panels_helper.rb
+++ app/helpers/panels_helper.rb
@@ -4,7 +4,7 @@ module PanelsHelper
     return {
       "titleCaption" => ["Cover Screen: 1 image", 1],
       "loadSingleImage" => ["Full Screen: 1 image", 1],
-      "titleCaptionWithOVerflow" => ["Single image with Caption: 1 image", 1],
+      "titleCaptionWithOverflow" => ["Single image with Caption: 1 image", 1],
       "loadTwoImages" => ["Split Screen: 2 images", 2],
       "niceView4Images" => ["Grid: 4 images", 4],
       "imagesWithCaptions" => ["Captioned Grid: 8 images", 8],
