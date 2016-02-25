cf3e4667c3e0e2eb45408de02012f0f57cc83a7b
David Ma <email": null>
2016-02-24 23:24:45 -0600

    Fix precompile

--- app/assets/javascripts/views/experiences/experience_view.js
+++ app/assets/javascripts/views/experiences/experience_view.js
@@ -79,7 +79,7 @@ ExperienceView.prototype.loadRemainingAssets = function(startingIndex) {
 
 ExperienceView.prototype.render = function() {
   removePanelNavigation()
-  For testing a predefined set of routes
+  // For testing a predefined set of routes
   this.loadAssets(["loadSingleImage", "loadTwoImages"])
   this.loadAssets([
     ["titleCaptionWithOverflow", 4], //Includes overflow and "loadSingleImage" should be next
