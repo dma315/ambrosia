cf6b84f9e368aab78a09b40419680a3907bfdbc5
David Ma <email": null>
2016-02-25 16:34:59 -0600

    Precompile and fix single image

--- app/assets/javascripts/views/experiences/panel_view.js
+++ app/assets/javascripts/views/experiences/panel_view.js
@@ -5,7 +5,7 @@ function PanelView(assetsArray) {
 PanelView.prototype.loadSingleImage = function() {
   var src = this.assets[0].direct_upload_url
   var $div = $("<div>").addClass("section")
-  var $img = $("<img>").attr("src", src)
+  var $img = $("<img>").attr("src", src).addClass("load-single-image")
   $div.append($img)
   return $div
 }
