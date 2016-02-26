a76bf39a9512d4228691aa271f62f797ac33cb78
David Ma <email": null>
2016-02-26 12:58:44 -0600

    Caption works now

--- app/assets/javascripts/views/assets/submit_caption.js
+++ app/assets/javascripts/views/assets/submit_caption.js
@@ -2,6 +2,20 @@ function submitCaption(form) {
   var $thisForm = $(form)
   var data = $thisForm.serialize();
   var url = $thisForm.attr('action')
+
+  var assetID = +$thisForm.closest('.edit-image-caption-container').attr('id')
+  var thisExperience = EXPERIENCES.find(function(experience) {
+    return experience.assets.find(function(asset) {
+      return asset.id === assetID
+    })
+  })
+  var thisAsset = thisExperience.assets.find(function(asset) {
+    return asset.id === assetID
+  })
+  var caption = $thisForm.find('#assets_caption').val()
+  thisAsset.caption = caption
+
+  // Update remotely
   $.ajax({
     method: "patch",
     url: url,
@@ -9,7 +23,7 @@ function submitCaption(form) {
   })
   .done(function(response) {
     var textToInject = response.replace(/"/g,"")
-    $thisForm.find('.click-to-edit-text').text(textToInject)    
+    $thisForm.find('.click-to-edit-text').text(textToInject)
     $thisForm.find('.edit-image-form').fadeOut();
   })
 }
